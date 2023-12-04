1ebc956bca03eb2376e34658c1c6c1db82c61a1e
shibzuko <shibzuko@gmail.com>
2023-12-04 23:08:18 +0300

    alpha-1.0

--- js/app.js
+++ js/app.js
@@ -1,53 +1,60 @@
-let tg = window.Telegram.WebApp;
-tg.expand();
-
-// Изменяем стиль основной кнопки
-tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "#2A9D8F";
-
-// Обработка отправки формы
-document.getElementById("registrationForm").addEventListener("submit", function(e) {
-    console.log("Form submitted");
-    e.preventDefault();
-
-    const form = e.target;
-
-    // Собираем данные о родителе
-    const parentName = form['parentName'].value;
-    const phoneNumber = form['phoneNumber'].value;
-    const childrenCount = form['childrenCount'].value;
-
-    // Собираем данные о детях
-    const childrenInfoElements = form.querySelectorAll('.child-info-block');
-    const children = Array.from(childrenInfoElements).map(childBlock => {
-        return {
-            name: childBlock.querySelector('[name="childName[]"]').value,
-            age: childBlock.querySelector('[name="childAge[]"]').value,
-            class: childBlock.querySelector('[name="childClass[]"]').value
-        };
-    });
-
-    // Формируем объект сообщения
-    const messageObj = {
-        'Parent Name': parentName,
-        'Phone Number': phoneNumber,
-        'Children Count': childrenCount,
-        'Children Info': children
-    };
-
-    // Преобразуем объект сообщения в строку JSON
-    const message = JSON.stringify(messageObj);
-
-    // Отправляем данные в Telegram
-    tg.sendData(message);
-    tg.MainButton.setText("Форма отправлена!");
-    tg.MainButton.show();
-});
-
-
+//let tg = window.Telegram.WebApp;
+//tg.expand();
+//
+//// Изменяем стиль основной кнопки
+//tg.MainButton.textColor = "#FFFFFF";
+//tg.MainButton.color = "#2A9D8F";
+//
+//// Обработка отправки формы
+//document.getElementById("registrationForm").addEventListener("submit", function(e) {
+//    console.log("Form submitted");
+//    e.preventDefault();
+//
+//    const form = e.target;
+//
+//    // Собираем данные о родителе
+//    const parentName = form['parentName'].value;
+//    const phoneNumber = form['phoneNumber'].value;
+//    const childrenCount = form['childrenCount'].value;
+//
+//    // Собираем данные о детях
+//    const childrenInfoElements = form.querySelectorAll('.child-info-block');
+//    const children = Array.from(childrenInfoElements).map(childBlock => {
+//        return {
+//            name: childBlock.querySelector('[name="childName[]"]').value,
+//            age: childBlock.querySelector('[name="childAge[]"]').value,
+//            class: childBlock.querySelector('[name="childClass[]"]').value
+//        };
+//    });
+//
+//    // Формируем объект сообщения
+//    const messageObj = {
+//        'Parent Name': parentName,
+//        'Phone Number': phoneNumber,
+//        'Children Count': childrenCount,
+//        'Children Info': children
+//    };
+//
+//    // Преобразуем объект сообщения в строку JSON
+//    const message = JSON.stringify(messageObj);
+//
+//    // Отправляем данные в Telegram
+//    tg.sendData(message);
+//    tg.MainButton.setText("Форма отправлена!");
+//    tg.MainButton.show();
+//});
 
 
 
+
+// Преобразуем объект сообщения в строку JSON
+const message = JSON.stringify(messageObj);
+
+// Выводим сформированное сообщение в консоль для отладки
+console.log("Sending message:", message);
+
+// Отправляем данные в Telegram
+tg.sendData(message);
 
 
 
